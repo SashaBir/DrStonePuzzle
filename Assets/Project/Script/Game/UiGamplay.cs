7e6c725c070b8a1c2dda82cf260dd2a2cbe12062
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-05-05 13:53:58 +0300

    add ability exit from puzzle

--- Assets/Project/Script/Game/UiGamplay.cs
+++ Assets/Project/Script/Game/UiGamplay.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Puzzle
-{//
+{
     public class UiGamplay : MonoBehaviour
     {
         [Header("Logic")]
@@ -12,17 +13,29 @@ namespace Puzzle
         [Header("Ui")]
         [SerializeField] private Button _menuButton;
         [SerializeField] private Button _restartButton;
+        [SerializeField] private GameObject _selfPuzzle;
+        [SerializeField] private GameObject _puzzlePanel;
+        [SerializeField] private GameObject _listLevelPanel;
 
         private void OnEnable()
         {
-            _menuButton.onClick.AddListener(_shutdowner.ShutdownGame);
+            _menuButton.onClick.AddListener(OnExit);
             _restartButton.onClick.AddListener(_restarter.RestartGame);
         }
 
         private void OnDisable()
         {
-            _menuButton.onClick.RemoveListener(_shutdowner.ShutdownGame);
+            _menuButton.onClick.RemoveListener(OnExit);
             _restartButton.onClick.RemoveListener(_restarter.RestartGame);
+        }
+
+        private void OnExit()
+        {
+            _shutdowner.ShutdownGame();
+
+            _selfPuzzle.SetActive(false);
+            _puzzlePanel.SetActive(false);
+            _listLevelPanel.SetActive(true);
         }
     }
 }
